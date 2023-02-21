54eb259b885d973f782119f97eec09683646dd86
farhanfahidurrahim <farhan.fahidurrahim@gmail.com>
2023-02-21 15:47:21 +0600

    about,privacy,help -api + restaurnt status api & popular deal column add

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Restaurant extends Model
 {
     use HasFactory;
-    protected $fillable = ['name','division_id','location','description','discount','latitude','longitude','contact_no','facebook_page','website_link','youtube_link','photo','tags','status'];
+    protected $fillable = ['name','division_id','location','description','discount','latitude','longitude','contact_no','facebook_page','website_link','youtube_link','photo','tags','status','popular_deal'];
 }
